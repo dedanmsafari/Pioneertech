b85075ed51c8903b65d2a430081f7e052e353e4d
dedanmsafari <dedan.msafari@gmail.com>
2021-07-19 21:44:24 +0300

    updated the app.js file for the about routes

---
+++ src/components/App.js
@@ -0,0 +1,120 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './ui/Theme';
+
+import Header from './ui/Header';
+import Footer from './ui/Footer';
+import LandingPage from './LandingPage';
+import ServicesPage from './ServicesPage';
+import CustomSoftware from './CustomSoftware';
+import MobileApps from './MobileApps';
+import Websites from './Websites';
+import Revolution from './Revolution';
+import About from './About';
+
+function App() {
+  const [value, setValue] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header
+          value={value}
+          setValue={setValue}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={(props) => (
+              <LandingPage
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/services'
+            render={(props) => (
+              <ServicesPage
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/customsoftware'
+            render={(props) => (
+              <CustomSoftware
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/mobileapps'
+            render={(props) => (
+              <MobileApps
+                {...props}
+                setSelectedIndex={setSelectedIndex}
+                setValue={setValue}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/websites'
+            render={(props) => (
+              <Websites
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/revolution'
+            render={(props) => (
+              <Revolution
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/about'
+            render={(props) => (
+              <About
+                {...props}
+                setValue={setValue}
+                setSelectedIndex={setSelectedIndex}
+              />
+            )}
+          />
+          <Route
+            exact
+            path='/contact'
+            component={() => <div>Contact Us</div>}
+          />
+          <Route exact path='/estimate' component={() => <div>Estimate</div>} />
+        </Switch>
+        <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
