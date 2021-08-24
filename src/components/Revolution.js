090181db529a6499e11677e62a2c9589a8d96cea
dedanmsafari <dedan.msafari@gmail.com>
2021-08-24 14:41:54 +0300

    modified the revolutions page

--- src/components/Revolution.js
+++ src/components/Revolution.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 import Lottie from 'react-lottie';
 import {
   Grid,
-  Hidden,
-  IconButton,
+
   useMediaQuery,
   makeStyles,
   useTheme,
